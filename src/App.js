0ab7da6011c385db92f2a480b6d0bc789f7d86e1
Luis Barreras <luis.barreras@unosquare.com>
2019-09-24 17:20:31 -0700

    Chapter Routing Lesson 14 'Exercises - NavBar and Routing'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import Movies from './components/movies';
+import { Route, Switch, Redirect } from "react-router-dom";
+import Movies from "./components/movies";
+import MovieDetails from "./components/movieDetails"
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NavBar from './components/navBar';
+import NotFound from './components/notFound';
 
 class App extends Component {
 
   render() {
     return (
       <main className="container">
-        <Movies />
-      </main>);
+        <NavBar />
+        <Switch>
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+
+          <Route path="/not-found" exact component={NotFound} />
+          <Route path="/" exact component={Movies} />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    );
   }
 }
 
